422a99598bc994076bb0013df4a8148dff172c31
Piotr Rusin <piotr.rusin88@gmail.com>
2019-02-03 04:47:10 +0100

    Add a jinja2 filter for grouping values by year

---
+++ templatetags/filters.py
@@ -0,0 +1,9 @@
+from statik.templatetags import register
+from collections import defaultdict
+
+@register.filter(name='group_by_year')
+def group_by_year(values):
+    values_by_year = defaultdict(list)
+    for v in values:
+        values_by_year[v.date.year].append(v)
+    return values_by_year.items()
